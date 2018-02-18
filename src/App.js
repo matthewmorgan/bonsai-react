1c363d2a3c05fcab6113767fca83f10bbbfbd128
Matthew Morgan <matt@dulcetsoftware.com>
2018-02-18 17:53:13 -0500

    Working Bonsai modal

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Modal from './modal';
+import Modal from './Modal';
 
 
 class App extends Component {
 
   constructor(props){
     super(props);
-    this.state = {modalIsVisible: false, ...this.props};
+    this.state = {visible: false, ...this.props};
     this.handleModalClick=this.handleModalClick.bind(this);
   }
 
   handleModalClick() {
-    this.setState({modalIsVisible: true})
+    this.setState({visible: true})
   }
   render() {
-    const modalBody = (
-        <div>
-          <p>Here's some body text.</p>
-        </div>
-    );
+
     return (
       <div className="App">
         <header className="App-header">
@@ -30,8 +26,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <button className="button" onClick={this.handleModalClick}>Show a modal</button>
-        <Modal isVisible={this.state.modalIsVisible} modalTitle="This is not a Bonsai modal" modalBody={modalBody}></Modal>
+        <button type="button" className="button" data-reveal data-open="default-modal">Click me for a modal!</button>
+        <Modal data-nothing/>
       </div>
     );
   }
