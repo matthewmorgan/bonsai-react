dd5beec75b59fa1448ba6910ca8cd5987ff0ceb8
Matthew Morgan <matt@dulcetsoftware.com>
2018-02-18 20:09:07 -0500

    Add some comments to explain weird stuff

--- src/Modal.js
+++ src/Modal.js
@@ -4,6 +4,8 @@ import {BonsaiModal} from '@ithaka/bonsai/js/bonsai.modal'
 
 class Modal extends React.Component {
 
+  // manipulate the DOM _after_ React has done its work
+  // this attaches the Foundation listeners to the appropriate controls (by class or id) in the DOM
   componentDidMount() {
     new BonsaiModal(window.$('.modal'))
   }
@@ -16,6 +18,7 @@ class Modal extends React.Component {
                data-circle-color="alert">
             <h1>{this.props.title}</h1>
             <p>{this.props.bodyText}</p>
+            {/* need the verbose data-close="" to work around an incompatibility between React and Foundation */}
             <button className="close-button" data-close="" aria-label="Close modal" type="button">
                 <span aria-hidden="true">&times;</span>
             </button>
